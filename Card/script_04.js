37a2332bd8e111a13bd066c367bdc55b64f50097
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-04-17 22:53:29 +0530

    script_04.js

--- Card/script_04.js
+++ Card/script_04.js
@@ -20,34 +20,44 @@ let arr = [
         istatus : "Stranger"
     },
     {
-      user: "rahul",
-     age : 30,
-     img : "https://images.unsplash.com/photo-1627060770557-e5d4581f6b61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNhbG1hbiUyMGtoYW58ZW58MHx8MHx8fDA%3D",
-     marid : "married",
-     istatus : "Stranger"
+        user: "rahul",
+        age : 30,
+        img : "https://images.unsplash.com/photo-1627060770557-e5d4581f6b61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHNhbG1hbiUyMGtoYW58ZW58MHx8MHx8fDA%3D",
+        marid : "married",
+        istatus : "Stranger"
     }
-]
+];
+
 function heroFunction(){
     let sum = '';
-arr.forEach(function(element,idx){
- sum +=  ` <div class="main">
- <div class="card">
-        <img src="${element.img} " alt="">
-        <h1>${element.user}</h1>
-        <h5>${element.age}</h5>
-        <h4>${element.marid}</h4>
-        <h5>${element.istatus}</h5>
-        <button id = ${idx}>Add Friend</button>
-    </div></div>`
-})    
-let body = document.querySelector('body');
-body.innerHTML = sum;
+    arr.forEach(function(element, idx){
+        sum +=  ` 
+        <div class="main">
+            <div class="card">
+                <img src="${element.img}" alt="">
+                <h1>${element.user}</h1>
+                <h5>${element.age}</h5>
+                <h4>${element.marid}</h4>
+                <h5>${element.istatus}</h5>
+                <button class="add-btn" data-index="${idx}">Add Friend</button>
+            </div>
+        </div>`;
+    });    
 
+    document.body.innerHTML = sum;
+
+    document.querySelectorAll('.add-btn').forEach(btn => {
+        btn.addEventListener('click', function() {
+            let index = this.getAttribute('data-index');
+            if(arr[index].istatus == "Stranger"){
+                arr[index].istatus = "Friend";
+            }
+            else{
+                arr[index].istatus = "Stranger";
+            }
+            heroFunction(); 
+        });
+    });
 }
 
 heroFunction();
-main.addEventListener('click',function(dets){
-    var gold = arr[dets.target.id];
-    gold.istatus = "Friend";
-    heroFunction();
-})
