e00d7e133e91fe4d1ecc417b9818ae52d30b1f2b
Kunal Choudhary <142143181+KunalChoudhary03@users.noreply.github.com>
2025-12-30 16:47:27 +0530

    Can send email using node js

---
+++ Nodemailer/index.js
@@ -0,0 +1,3 @@
+const sendEmail = require('./email');
+
+sendEmail("kunalchoudharyhack@gmail.com","Test Subject" , "This is a test email sent using Nodemailer with OAuth2 authentication.", "<h1>This is a test email sent using Nodemailer with OAuth2 authentication.</h1>");
